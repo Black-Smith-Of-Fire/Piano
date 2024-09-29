91f5d4ae14b2e0a85d8e3d413a835ad16b698436
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-09-29 09:30:53 +0530

    Class to test code

---
+++ NewPiano/src/TestMe.java
@@ -0,0 +1,12 @@
+// This class is for testing code purposes
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.util.Scanner;
+
+public class TestMe extends Thread {
+}
