cc8372a514252ee5847497429da410246f2c0113
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-13 10:19:54 +0530

    Running from here

--- NewPiano/src/Main.java
+++ NewPiano/src/Main.java
@@ -1,10 +1,28 @@
 // This class is for testing code purposes
 package src;
 
-public class Main {
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
+public class Main  {
+
+//    public void nativeKeyPressed(NativeKeyEvent e) {
+//       String lol = NativeKeyEvent.getKeyText(e.getKeyCode());
+//        System.out.println("Key pressed is : " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+//    }
     public static void main(String[] args) {
+//        try {
+//            GlobalScreen.registerNativeHook();
+//        }
+//        catch (NativeHookException e){
+//            System.err.println("There was a stupid problem");
+//            System.err.println(e.getMessage());
+//            System.exit(1);
+//        }
+//        GlobalScreen.addNativeKeyListener(new Main());
         new Piano();
-        // Until user inputs Q , this loop will run.
     }
+
 }
