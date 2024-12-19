9d467bb2cdde112cd6e320a67cee72ab549bd8dd
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-19 05:37:51 +0530

    Class to store the key and chord parameter

---
+++ Main/org/blacksmith/piano/KeyAndChords.java
@@ -0,0 +1,12 @@
+package org.blacksmith.piano;
+
+public class KeyAndChords {
+
+    int keys;
+    String chords;
+
+     KeyAndChords(int keys, String chords){
+        this.keys = keys;
+        this.chords = chords;
+    }
+}
